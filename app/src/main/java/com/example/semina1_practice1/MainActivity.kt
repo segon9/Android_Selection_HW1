f1d3ce39a3b16d1c5232755c76577ca47d17d51d
segon9 <qkrtprhs@gmail.com>
2020-04-24 14:56:20 +0900

    선택과제1 입니다

---
+++ app/src/main/java/com/example/semina1_practice1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.semina1_practice1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
